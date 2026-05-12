45202c64a01d84b8258ae97b7aae8eb78afcfde3
Michael Buschmann <hey@michael-buschmann.dev>
2026-05-12 19:39:37 +0200

    chore: stop tracking generated artifacts

--- backend/dist/db/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-const globalWithPrisma = globalThis;
-exports.prisma = globalWithPrisma.__prisma ?? new client_1.PrismaClient();
-if (process.env.NODE_ENV !== "production") {
-    globalWithPrisma.__prisma = exports.prisma;
-}
